07d8d9f8898d6d19031d0ba6aaf647d1eff216ca
Paul Thomson <paulthomson@users.noreply.github.com>
2019-10-18 09:59:33 +0100

    Only release bin/ (#7)

--- build.sh
+++ build.sh
@@ -137,6 +137,8 @@ for f in "${INSTALL_DIR}/bin/"*; do
   echo "${BUILD_REPO_SHA}">"${f}.build-version"
   cp "${WORK}/COMMIT_ID" "${f}.version"
 done
+rm -rf "${INSTALL_DIR:?}/lib"
+rm -rf "${INSTALL_DIR:?}/include"
 ###### END EDIT ######
 
 GRAPHICSFUZZ_COMMIT_SHA="b82cf495af1dea454218a332b88d2d309657594d"
